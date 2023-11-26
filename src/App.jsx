5a886d4b8c9aea44d6bd36254a7292152351718b
MehrG <akbarimehrgan@yahoo.com>
2023-11-26 11:49:26 +0330

    push on GitHub

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
+
+function App() {
+  const [activeTab, setActiveTab] = useState(1);
+  const heanleActiveTab = (id) => {
+    setActiveTab(id);
+  };
+
+  return (
+    <div className="tab">
+      <div className="tab__header">
+        {tabData.map((tab) => (
+          <button
+            onClick={() => heanleActiveTab(tab.id)}
+            key={tab.id}
+            className={activeTab === tab.id ? "active" : ""}
+          >
+            <span>{tab.title}</span>
+            <span className="tab__indicator"></span>
+          </button>
+        ))}
+      </div>
+      <div className="teb__content">{tabData[activeTab - 1].content}</div>
+    </div>
+  );
+}
+
+export default App;
